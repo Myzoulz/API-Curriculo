e17c99511970a876a948514701fdca72239ab8c6
Myzoulz <lucasrocha7455@gmail.com>
2025-06-16 17:30:35 -0300

    correcao usuario metodo put

--- src/main/java/com/myzoul/curriculo/util/PermissaoUtils.java
+++ src/main/java/com/myzoul/curriculo/util/PermissaoUtils.java
@@ -1,12 +1,12 @@
 package com.myzoul.curriculo.util;
 
-import com.myzoul.curriculo.exception.BusinessException;
 import com.myzoul.curriculo.model.CurriculoEnt;
+import org.springframework.security.access.AccessDeniedException;
 
 public class PermissaoUtils {
     public static void checarPermissaoCpf(CurriculoEnt curriculo, String cpfUsuario, String mensagemErro) {
-        if (!curriculo.getCpf().equals(cpfUsuario)) {
-            throw new BusinessException(mensagemErro);
+        if (!curriculo.getCpf().trim().equalsIgnoreCase(cpfUsuario.trim())) {
+            throw new AccessDeniedException(mensagemErro);
         }
     }
 }

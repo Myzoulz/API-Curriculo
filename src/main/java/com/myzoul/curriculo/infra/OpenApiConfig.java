c61582662675031bd1027fe7ad6cdb7de380674b
Myzoulz <lucasrocha7455@gmail.com>
2025-06-16 13:23:39 -0300

    update

--- src/main/java/com/myzoul/curriculo/infra/OpenApiConfig.java
+++ src/main/java/com/myzoul/curriculo/infra/OpenApiConfig.java
@@ -1,11 +1,14 @@
-package com.myzoul.curriculo.config;
+package com.myzoul.curriculo.infra;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -21,6 +24,9 @@ public class OpenApiConfig {
                                         .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(
+                        new Server().url("https://api-curriculo-production.up.railway.app")
+                ));
     }
 }

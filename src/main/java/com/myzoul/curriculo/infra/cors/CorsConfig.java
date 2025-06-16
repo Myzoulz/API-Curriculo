a1731feeb551c94125fb5909d3d4b81db8b55a68
Myzoulz <lucasrocha7455@gmail.com>
2025-06-16 12:55:56 -0300

    ajuste de cors

--- src/main/java/com/myzoul/curriculo/infra/cors/CorsConfig.java
+++ src/main/java/com/myzoul/curriculo/infra/cors/CorsConfig.java
@@ -9,7 +9,12 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200", // seu frontend local
+                        "http://localhost:8080", // swagger local
+                        "front-em-producao.com", // front produção
+                        "https://api-curriculo-production.up.railway.app/swagger-ui/index.html#/" // swagger produção, se aplicável
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
